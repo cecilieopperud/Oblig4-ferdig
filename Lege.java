8007422fb736f5f62fbe46661a49c8ff8222445b
Benjamintre <62058948+Benjamintre@users.noreply.github.com>
2020-03-27 18:19:17 +0100

    Update Lege.java

--- Lege.java
+++ Lege.java
@@ -6,7 +6,7 @@ class Lege implements Comparable<Lege> { //Oppretter klassen Lege.
 
   }
 
-  public int hentKontrollID() {
+  public int hentKontrollID() { //Metode for aa returnere 0 for lege.
     return 0;
   }
 
@@ -19,59 +19,59 @@ class Lege implements Comparable<Lege> { //Oppretter klassen Lege.
   }
 
   @Override
-  public int compareTo(Lege lege){
+  public int compareTo(Lege lege){ 
     return (legeNavn.compareTo(lege.hentNavn()));
   }
 
-  public Lenkeliste<Resepter> hentUtResepter(){
+  public Lenkeliste<Resepter> hentUtResepter(){ //Returnerer de utskrevede reseptene til legen.
     return utskrevedeResepter;
   }
 
-  public HvitResept skrivHvitResept(Legemiddel legemiddel, Pasient pasient, int reit) throws UlovligUtskrift{
+  public HvitResept skrivHvitResept(Legemiddel legemiddel, Pasient pasient, int reit) throws UlovligUtskrift{ //Oppretter hvit resept.
     HvitResept hResept = new HvitResept(legemiddel , this , pasient, reit);
-    if (hResept.hentLegemiddel() instanceof Narkotisk) {
-      throw new UlovligUtskrift(this, legemiddel, pasient.hentID());
+    if (hResept.hentLegemiddel() instanceof Narkotisk) { //Dersom det er narkotisk.
+      throw new UlovligUtskrift(this, legemiddel, pasient.hentID());  //Kaster unntaket UlovligUtskrift.
     }
-    else{
+    else{ //Hvis ikke legges reseptet til i legens utskrevede resepter og pasientens resepter.
       utskrevedeResepter.leggTil(hResept);
       pasient.leggTilResept(hResept);
       return hResept;
     }
   }
 
-  public MilaeterResept skrivMillitaerResept(Legemiddel legemiddel, Pasient pasient, int reit) throws UlovligUtskrift {
+  public MilaeterResept skrivMillitaerResept(Legemiddel legemiddel, Pasient pasient, int reit) throws UlovligUtskrift { //Oppretter hvit resept.
     MilaeterResept mResept = new MilaeterResept(legemiddel, this, pasient, reit);
-    if (mResept.hentLegemiddel() instanceof Narkotisk) {
-      throw new UlovligUtskrift( this, legemiddel, pasient.hentID());
+    if (mResept.hentLegemiddel() instanceof Narkotisk) { //Dersom det er narkotisk.
+      throw new UlovligUtskrift( this, legemiddel, pasient.hentID()); //Kaster unntaket UlovligUtskrift.
     }
-    else{
+    else{ //Hvis ikke legges reseptet til i legens utskrevede resepter og pasientens resepter.
       utskrevedeResepter.leggTil(mResept);
       pasient.leggTilResept(mResept);
       return mResept;
     }
   }
 
-  public PResept skrivPResept(Legemiddel legemiddel, Pasient pasient) throws UlovligUtskrift{
+  public PResept skrivPResept(Legemiddel legemiddel, Pasient pasient) throws UlovligUtskrift{ //Oppretter hvit resept.
     PResept pResept = new PResept(legemiddel , this, pasient);
-      if (pResept.hentLegemiddel() instanceof Narkotisk){
-        throw new UlovligUtskrift( this, legemiddel, pasient.hentID());
+      if (pResept.hentLegemiddel() instanceof Narkotisk){ //Dersom det er narkotisk.
+        throw new UlovligUtskrift( this, legemiddel, pasient.hentID()); //Kaster unntaket UlovligUtskrift.
       }
-      else{
+      else{ //Hvis ikke legges reseptet til i legens utskrevede resepter og pasientens resepter.
         utskrevedeResepter.leggTil(pResept);
         pasient.leggTilResept(pResept);
         return pResept;
       }
     }
 
-  public BlaaResept skrivBlaaResept(Legemiddel legemiddel, Pasient pasient, int reit) throws UlovligUtskrift {
+  public BlaaResept skrivBlaaResept(Legemiddel legemiddel, Pasient pasient, int reit) throws UlovligUtskrift { //Oppretter hvit resept.
 
     BlaaResept bResept = new BlaaResept(legemiddel , this , pasient, reit);
 
-    if (bResept.hentLegemiddel() instanceof Narkotisk) {
-      throw new UlovligUtskrift(this, legemiddel, pasient.hentID());
+    if (bResept.hentLegemiddel() instanceof Narkotisk) { //Dersom det er narkotisk.
+      throw new UlovligUtskrift(this, legemiddel, pasient.hentID()); //Kaster unntaket UlovligUtskrift.
     }
 
-    else{
+    else{ //Hvis ikke legges reseptet til i legens utskrevede resepter og pasientens resepter.
       utskrevedeResepter.leggTil(bResept);
       pasient.leggTilResept(bResept);
       return bResept;
